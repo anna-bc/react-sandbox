ab1151dd7ad6253f76dd77aa38c1221a0e2a8b22
Anna Lepak <anna.lepak@adesso.de>
2023-02-06 17:23:30 +0100

    added client-side form required validation

--- my-blog/src/components/PostForm/PostForm.js
+++ my-blog/src/components/PostForm/PostForm.js
@@ -20,10 +20,10 @@ export default function PostForm(props) {
     <>
     <div className='newPost'>Add a new Post</div>
     <form className="formWrapper" onSubmit={onSubmit}>
-      <input type="text" className="form__item title" placeholder="Write a Title" onInput={e => setPost({...post, title: e.target.value})} />
-      <textarea className="form__item content" placeholder="Write some content in here" onInput={e => setPost({...post, content: e.target.value})} />
+      <input type="text" className="form__item title" placeholder="Write a Title" required onInput={e => setPost({...post, title: e.target.value})} />
+      <textarea className="form__item content" placeholder="Write some content in here" required onInput={e => setPost({...post, content: e.target.value})} />
       <div className="moderationWrapper">
-        <input type="text" className="moderation__item author" placeholder='who wrote this post?' onInput={e => setPost({...post, author: e.target.value})} />
+        <input type="text" className="moderation__item author" placeholder='who wrote this post?' required onInput={e => setPost({...post, author: e.target.value})} />
         <div className="moderation__item buttons">
           <input type="submit" className="buttons__item" />
         </div>
