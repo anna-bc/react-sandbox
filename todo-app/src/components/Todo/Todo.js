fbbf9f329424d9f74f18a10b62e030b9ae9fee41
Anna Lepak <anna.lepak@adesso.de>
2023-02-06 09:27:07 +0100

    added priority styling

--- todo-app/src/components/Todo/Todo.js
+++ todo-app/src/components/Todo/Todo.js
@@ -3,9 +3,9 @@ import "./Todo.css";
 function Todo(props) {
   console.log(props);
   return (
-    <div className="todoItem">
+    <div className={"todoItem todoItem--" + props.todo.priority.value}>
       <div className="todoTitle">{props.todo.title}</div>
-      <div className="todoInfos">
+      <div className={"todoInfos todoInfos--" + props.todo.priority.value}>
         <div className="todoInfos__item todoInfos__item--dueDate">
           {props.todo.dueDate}
         </div>
