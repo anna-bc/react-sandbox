286444962a81b2236c3f6418a102d193d9ab4a36
Anna Lepak <anna.lepak@adesso.de>
2023-02-07 11:18:46 +0100

    added persistence with local storage

--- my-blog/src/components/Post/Post.js
+++ my-blog/src/components/Post/Post.js
@@ -1,6 +1,7 @@
 import "./Post.css";
 
 import { useState } from "react";
+import PostForm from "../PostForm/PostForm";
 
 export default function Post(props) {
   const [post, setPost] = useState({
@@ -10,20 +11,24 @@ export default function Post(props) {
   });
 
   function handleDeleteClick() {
-    props.deleteClick(props.post)
+    props.deleteClick(props.post);
   }
 
   return (
-    <div className="postWrapper">
-      <div className="post__item title">{props.post.title}</div>
-      <div className="post__item content"> {props.post.content} </div>
-      <div className="moderationWrapper">
-        <div className="moderation__item author">{props.post.author}</div>
-        <div className="moderation__item buttons">
-          <button className="buttons__item">Edit</button>
-          <button className="buttons__item" onClick={handleDeleteClick}>Delete</button>
+    <>
+      <div className="postWrapper">
+        <div className="post__item title">{props.post.title}</div>
+        <div className="post__item content"> {props.post.content} </div>
+        <div className="moderationWrapper">
+          <div className="moderation__item author">{props.post.author}</div>
+          <div className="moderation__item buttons">
+            <button className="buttons__item" id="editBtn">Edit</button>
+            <button className="buttons__item" id="deletBtn" onClick={handleDeleteClick}>
+              Delete
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
