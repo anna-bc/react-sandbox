ac634d8abe53e7635c62fee7862f768ef6052b0f
Anna Lepak <anna.lepak@adesso.de>
2023-02-10 16:41:38 +0100

    added some styling to comments

--- my-blog/src/components/CommentsList/CommentsList.js
+++ my-blog/src/components/CommentsList/CommentsList.js
@@ -1,13 +1,14 @@
 import React from "react";
+import './CommentsList.scss';
 import Comment from "../Comment/Comment";
 
 function CommentsList(props) {
   return (
     <div className={"CommentsList" + " CommentsList--" + props.theme}>
-      <ul>
+      <ul id="commentList">
         {props.comments.map((comment, i) => (
           <li key={i}>
-            <Comment author={comment.author} content={comment.content} />
+            <Comment theme={props.theme} author={comment.author} content={comment.content} />
           </li>
         ))}
       </ul>
